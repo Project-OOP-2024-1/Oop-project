2059eb7248d30e79c49c1a11e2a1bc7b3f46caa0
Kiet <minhkiet200504@gmail.com>
2024-11-18 17:05:46 +0700

    Shield and Sword

---
+++ src/object/OBJ_Shield.java
@@ -0,0 +1,19 @@
+package object;
+
+import entity.Entity;
+import main.GamePanel;
+import sprite.SpriteSheet;
+
+public class OBJ_Shield extends Entity {
+    public OBJ_Shield(GamePanel gp){
+        super(gp);
+        name="shield";
+        try {
+            SpriteSheet sheet = new SpriteSheet("/objects/shield.png", gp.originalTileSize, gp.originalTileSize);
+            image = sheet.getSprite(0, 0);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        defenseValue=1;
+    }
+}
---
+++ src/object/OBJ_Sword.java
@@ -0,0 +1,19 @@
+package object;
+
+import entity.Entity;
+import main.GamePanel;
+import sprite.SpriteSheet;
+
+public class OBJ_Sword extends Entity {
+    public OBJ_Sword(GamePanel gp){
+        super(gp);
+        name= "sword";
+        try {
+            SpriteSheet sheet = new SpriteSheet("/objects/sword.png", gp.originalTileSize, gp.originalTileSize);
+            image = sheet.getSprite(0, 0);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        damage=1;
+    }
+}
